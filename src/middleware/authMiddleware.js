7d079b6ded7e42661c9b45b048127e941e4f16df
Nguyen Hoang <89145285+hoangit2k2@users.noreply.github.com>
2023-12-10 21:55:46 +0700

    Update authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -25,7 +25,10 @@ const authMiddlleware = (req, res, next) =>{
 }
 const authUserMiddlleware = (req, res, next) =>{
 
-    const token = req.headers.token.split(' ')[1]
+    // const token = req.headers.token.split(' ')[1]
+    const token = req.headers.token
+    console.log('=================>' + token)
+   
     const userId = req.params.id
     jwt.verify(token, process.env.ACCESS_TOKEN, function(err, user){
         if(err){
